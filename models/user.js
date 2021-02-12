322619a295916d3fd6817ef24e1d691b542ed8ea
aharri64 <haveacompleteday@gmail.com>
2021-02-11 17:29:47 -0700

    did some more, still some errors

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 'use strict';
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const {
   Model
 } = require('sequelize');
@@ -18,22 +18,25 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       validate: {
-        len[1, 99],
-        msg: 'Name must be between 1 and 99 characters'
+        len: {
+          args: [1,99],
+          msg: 'Name must be between 1 and 99 characters'
+      
+        }
       }
     },
     email: {
       type: DataTypes.STRING,
       validate: {
-        isEmail: { //? this part is a validation, it has to check this first
-          msg: 'Invalide email'
+        isEmail: { // does a boolean check
+          msg: 'Invalid email'
         }
       }
     },
     password: {
       type: DataTypes.STRING,
       validate: {
-        len: [8, 99],
+        len: [8,99],
         msg: 'Password must be between 8 and 99 characters'
       }
     }
@@ -41,28 +44,29 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
-  return user;
+
+  // Before a user is created, we are encrypting the password and using hash in its place
+  user.addHook('beforeCreate', (pendingUser) => { // pendingUser is object that gets passed to DB
+    // Bcrypt is going to hash the password
+    let hash = bcrypt.hashSync(pendingUser.password, 12); //
+    pendingUser.password = hash; // this will go to the DB
+  });
+
+  // checking the password on Sign-In and comparing to the hashed password in the DB
+  user.prototype.validPassword = function(typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); // check to see if password is correct.
+    
+    return isCorrectPassword;
+  }
+
+  // return an object from the database of the user without the encrypted password
+  user.prototype.toJSON = function() {
+    let userData = this.get(); // 
+    delete userData.password; // it doesn't delete password from database, only removes it. 
+    
+    return userData;
+  }
+
+  return user; // above here
 };
 
-//* Before a user is created, we are encrypting the password and using hash in its place
-user.addHook('beforeCreate', (pendingUser) => { //? pedingUser is object that gets passed to DB
-  //? bcrypt is going to hash the password
-  let hash = bcrypt.hashSync(pendingUser.password, 12); //?what does pass mean? 
-  pendingUser.password = hash; //* this will go to the DB
-})
-
-//* checking the password on sign-in and comparing to the hashed password in the db
-user.prototype.validPassword = function (typedPassword) {
-  let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); //* check to see if password is correct
-  return is isCorrectPassword
-}
-
-//* return an object from the database pf the iser without the encrypted password
-user.prototype.toJSON = function () {
-  let userData = this.get(); //
-  delete userData.password; //* this does not delete from the database, only removes it
-  return userData;
-}
-
-
-return user; //above here
